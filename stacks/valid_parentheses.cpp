bef2368747e632ad07377c6f64fd090a71974a6f
benwilliamss <benwilliams1996@hotmail.co.uk>
2021-10-03 17:17:38 +0100

    a different a attempt that iterates through the whole string as opposed to considering the first and next element to find a respective bracket match

--- stacks/valid_parentheses.cpp
+++ stacks/valid_parentheses.cpp
@@ -18,79 +18,56 @@ class Solution {
 	 }
 		
 	 char popped_char;
-	 char front_char = s.front();
+	 char char_to_look_for; 
+	 bool respective_bracket_not_found;//stops checking anymore for matches when this flag is true
 	
 
 	 //pop char, compare if the corresponding bracket is either next or at the front
 	 while(!s.empty())
 	 {	
-	 	cout << "\n\n Current string: " <<  s << "\n" << endl;
+		 
+		 respective_bracket_not_found=true;
+
+	 	 //cout << "\n\n Current string: " <<  s << "\n" << endl;
 
 		 popped_char = s.back(); //get back element 
-		 s.pop_back(); //remove back element
-		 printf("popped_char %c\n", popped_char);
+		 s.pop_back(); //remove back element 
+		 //printf("popped_char %c\n", popped_char);
+
+
 			
 		 switch(popped_char)
 		 {
 				
 				case ')':
-					if(s.back() == '(')
-					{
-						s.pop_back();//paired elements now removed
-						break;
-					}
-					
-					else if(front_char== '(')
-					{	
-						printf("popped %c Matches front %c", popped_char, front_char);
-						//remove front element pair
-						s.erase(0,1);
-						front_char= s.front();
-						break;
-					}
-
-			       		return false; //no matching elements 
-
-
+					char_to_look_for = '('; 
+					break;
 
 				case ']': 
-					if(s.back() == '[')
-					{
-						s.pop_back();//paired elements now removed
-						break;
-					}
-
-					else if(front_char== '[')
-					{	printf("popped: %c Matches front: %c", popped_char, front_char);
-						//remove front element pair
-						s.erase(0,1);
-						front_char= s.front();
-						break;
-					}
-
-					return false;
-
+					char_to_look_for = '[';
+					break;
 
 				case '}':
-					if(s.back() == '{')
-					{
-						s.pop_back();//paired elements now removed
-						break;
-					}
-
-					else if(front_char== '{')
-					{
-						printf("popped: %c Matches front: %c", popped_char, front_char);
-						//remove front element pair
-						s.erase(0,1);
-						front_char= s.front();
-						break;
-					}
-
-			       		return false; 
-
+					char_to_look_for = '{'; 
+					break;
+				default:
+					//hit a left bracket
+					//which already should be removed by this point
+					//string not valid
+					return false;			
 							
 		 }
+		 
+		 for(int i= (s.length()-1); i>=0; i--){
+					 	
+		 	if(s[i] == char_to_look_for)
+			{	
+				respective_bracket_not_found = false; 
+				s.erase(i,1); //remove element, not considered again
+				break;
+			}
+		 } 
+		if(respective_bracket_not_found) return false;
 
 	 }
 	 return true; //string now empty as all brackets now have respective counterparts
@@ -102,11 +79,13 @@ class Solution {
 int main()
 {
 	Solution solution_one;
-	//printf("\nstring valid: %s", solution_one.isValid("{[]}") ? "true" : "false");
-	//	printf("\nstring valid: %s", solution_one.isValid("[(]") ? "true" : "false");
-	//	printf("\nstring valid: %s", solution_one.isValid("}[(){]") ? "true" : "false");
-	///printf("\n string valid: %s", solution_one.isValid("{}()[]") ? "true" : "false");
-	printf("\nstring valid: %s", solution_one.isValid("[[]{}}") ? "true" : "false");
+	
+	
+	//printf("\nstring valid: %s", solution_one.isValid("[({(())}[()])]") ? "true" : "false");
+	//printf("\nstring valid: %s", solution_one.isValid("((") ? "true" : "false");
+	//printf("\nstring valid: %s", solution_one.isValid("{({()[]})}") ? "true" : "false");
+	//printf("\n string valid: %s", solution_one.isValid("{}()[]") ? "true" : "false");
+	//printf("\nstring valid: %s", solution_one.isValid("[[]{}}") ? "true" : "false");
 
 
 
